4ae988d66893328e80fcecf45081b9e164f9b7be
Ricter Z <ricterzheng@gmail.com>
2026-01-15 11:57:58 +0800

    Export app from lexicon package for uvicorn lexicon:app usage

--- src/lexicon/__init__.py
+++ src/lexicon/__init__.py
@@ -4,7 +4,8 @@ FFXIV Equipment Auto-labeling Package
 
 from .labeler import FFXIVAutoLabeler
 from .label_system import get_prompt_template, get_all_labels
+from .app import app
 
 __version__ = "0.1.0"
-__all__ = ["FFXIVAutoLabeler", "get_prompt_template", "get_all_labels"]
+__all__ = ["FFXIVAutoLabeler", "get_prompt_template", "get_all_labels", "app"]
 
